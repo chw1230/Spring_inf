b437547d924571d5adeacf55aaceb987e8615937
chw1230 <choihw1230@naver.com>
2025-10-07 13:36:40 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 - ch04 - 메시지, 국제화 공부 ✏️

---
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,56 @@
+package hello.itemservice.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@SpringBootTest
+public class MessageSourceTest {
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null); // locale 정보가 없으면 basename에서 설정한 기본 이름 메시지 파일을 조회
+        // application.properties에서 messages.basename=messages message를 기본 이름 메시지 파일로 저장해서 기본 적으로 messages.properties 파일에서 데이터를 조회!
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+
+    }
+
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지"); // 메시지가 없으면 기본 메시지를 반환!
+    }
+
+    @Test
+    void argumentMessage() {
+        String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(result).isEqualTo("안녕 Spring");
+    }
+
+    // 국제화 적용
+    @Test
+    void defaultLang() {
+        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕"); // locale 정보가 없으므로 messages 를 사용
+        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕"); // locale 정보가 있지만, message_ko 가 없으므로 messages를 사용
+    }
+
+    @Test
+    void enLang() {
+        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello"); // locale 정보가 Locale.ENGLISH 이므로 messages_en 을 찾아서 사용
+    }
+}
